4877ebfbfa69d714086f4fdd12e1992e60558c5d
ryan <longinglove@nate.com>
2017-03-08 13:42:09 +0900

    de,it,es,fr,pl,fi added

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Pronunciation Dictionaries for multiple languages
+# Pronunciation Dictionaries for Multiple Languages
 
-Pronunciation dictionaries can be used for several purposes. Among them is Text-to-Speech. Generally speaking, you can't directly convert text to sound. Instead, you need to transform the text into pronunciation first. Because every language/script has its own pronouncing rules, ...
-
+Pronunciation dictionaries can be used for several purposes. For example, one can convert text to a sequence of phonemes using them. Not surprisingly, you can freely access to a great English pronunciation dictionary provided by [CMU](https://github.com/cmusphinx/cmudict), although the pronunciation symbols are Arpabets, not IPAs. Again not surprisingly, it's hard to get pronunciation dictionaries for other languages. One reason is that for several languages their scripts are phonetic more or less; the pronunciation of a word can be inferred by some rules. However, neither rules always can cover everything nor everybody knows the rules. Decent and open-source pronunciation dictionaries are still necessary.
 
 ## Requirements
 * lxml >= 3.3.3
+* python >= 3.4
 	
-## Background / References
+## References
 * [CMU US English pronouncing dictionary](https://github.com/cmusphinx/cmudict)
 
 ## Work Flow
@@ -14,7 +14,23 @@ Pronunciation dictionaries can be used for several purposes. Among them is Text-
 * STEP 2. Extract it to `data/raw/` folder.
 * STEP 3. Run `build_corpus.py`.
 
-## Pre-build dictionaries
-Click the name of the language to download its prebuilt pronunciation dictionaries.
+## Pre-built dictionaries
+Click the name of the language to download the prebuilt dictionary.
 
 * [German](https://dl.dropboxusercontent.com/u/42868014/pron_dicts/de.csv.tar.gz)
+* [Italian](https://dl.dropboxusercontent.com/u/42868014/pron_dicts/it.csv.tar.gz)
+* [Spanish](https://dl.dropboxusercontent.com/u/42868014/pron_dicts/es.csv.tar.gz)
+* [French](https://dl.dropboxusercontent.com/u/42868014/pron_dicts/fr.csv.tar.gz)
+* [Polish](https://dl.dropboxusercontent.com/u/42868014/pron_dicts/pl.csv.tar.gz)
+* [Finnish](https://dl.dropboxusercontent.com/u/42868014/pron_dicts/fi.csv.tar.gz)
+
+## Note
+
+Any contributions / error reports are welcome. We will keep adding languages.
+
+## Disclaimer
+
+We are not responsible for the content (headwords and pronunciations). They may contain mistakes and/or inconsistent use of symbols.
+
+
+
--- build_corpus.py
+++ build_corpus.py
@@ -1,6 +1,5 @@
+#!/usr/bin/python3
 # coding: utf-8
-#!/usr/bin/python2
-from __future__ import print_function
 import argparse
 import codecs
 import os
@@ -9,79 +8,89 @@ import re
 import lxml.etree as ET
 import unicodedata
 
-ns = "{http://www.mediawiki.org/xml/export-0.10/}" # namespace
+# namespace
+ns = "{http://www.mediawiki.org/xml/export-0.10/}" 
 
 # arguments setting 
 lcode = sys.argv[1]
 fname = "{}wiktionary-20161201-pages-articles-multistream.xml".format(lcode)    
 
-def _refine_hw(hw):
-    global lcode
-    
-    if lcode in ['de']:
-        return hw
-    
 def _refine_pron(pron):
-    global lcode
+    """Refines pronunciation (string)"""
+    # remove punctuations
+    pron = re.sub("[\u2000-\u206F.,·/#!$%\^&*;:{}=\-_`~()<>\[\]]", "", pron)
+
+    # unicode normalization
+    pron = unicodedata.normalize('NFD', pron)
     
-    if lcode in ['de']:
-        # remove punctuations
-        pron = re.sub(u"[.,·/#!$%\^&*;:{}=–\-_`~()<>]", "", pron)
-        
-        # unicode normalization
-        pron = unicodedata.normalize('NFD', pron)
-        
-        # digraph normalization
-        dict = {u"ʣ":u"dz", u"ʤ":u"dʒ", u"ʦ":u"ts", u"ʧ":u"tʃ"}
-        for k, v in dict.items():
-            pron = pron.replace(k, v)
-        
-        # split into phonemes
-        ## proness
-        pron = re.sub(ur"([ˈˌ])", ur" \1", pron).strip()
-        ## diacritics 
-        pron = re.sub(ur"([^\U00000300-\U0000036F\U000002B0-\U000002FF])", ur' \1', pron).strip()
-        
-        # squeeze spaces
-        pron = re.sub("[ ]+", " ", pron)
-        return pron
+    # digraph normalization
+    dict = {"ʣ":"dz", "ʤ":"dʒ", "ʦ":"ts", "ʧ":"tʃ"}
+    for k, v in dict.items():
+        pron = pron.replace(k, v)
+    
+    # split into single phonemes
+    ## stress symbols
+    pron = re.sub("[ʼ']", "ˈ", pron)
+    pron = re.sub("([ˈˌ])", r" \1", pron)
+    
+    ## diacritics must combine with their preceding letters.
+    pron = re.sub("([^\U00000300-\U0000036F\U000002B0-\U000002FF])", r' \1', pron)
 
-def _get_hws(elem):
-    '''Given elem, returns list of headwords'''
-    global ns, lcode
-    if lcode in ['de']: # There's only one form of headword.
-        try:
-            hw = elem.find('./{}title'.format(ns)).text
-            
-            # validity check
-            if " " in hw or "," in hw: 
-                return None
-            else:
-                return [_refine_hw(hw)]
-        except:
+    # squeeze spaces
+    pron = re.sub("[ ]+", " ", pron)
+    
+    return pron.strip()
+
+def _get_hw(elem):
+    '''Returns a headword (string) from elem (element)'''
+    try:
+        hw = elem.find('./{}title'.format(ns)).text
+        
+        # validity check
+        if " " in hw or "," in hw: # We will NOT cover multi-word headwords.
             return None
+        else:
+            return hw
+    except:
+        return None
 
+def _get_entry_block(text):
+    """Returns a relevant block (string) from text"""
+    ltags = {"de":"Sprache|Deutsch",
+             "es":"lengua|es",
+             "fi":"Suomi",
+             "fr":"langue|fr",
+             "it":"{{-it-}}",
+             "pl":"język polski",
+             }
+    ltag = ltags.get(lcode)
+    for entry_block in re.split("(?m)^==(?!=)", text):
+        if ltag in entry_block:
+            return entry_block
+    return None
+        
 def _get_prons(elem):
-    '''Given elem, returns list of pronunciations'''
-    global ns, lcode
-    if lcode in ['de']:
-        try:
-            text = elem.find('./{0}revision/{0}text'.format(ns)).text
-            if re.search(ur"^==[^=]+Sprache\|Deutsch", text) is None: # In german wiktionary, languages other than german are included.
-                return None
-            pron = re.search(ur"{{IPA}}[^\n]+Lautschrift\|([^}]+)", text).group(1)
-            
-            # validity check
-            if u"..." in pron or u"…" in pron:
-                return None
-            else:
-                return [_refine_pron(pron)]
-        except:
-            return None
-    
+    '''returns list of pronunciations from elem (element)'''
+    try:
+        text = elem.find('./{0}revision/{0}text'.format(ns)).text
+        entry_block = _get_entry_block(text)
+        if lcode in ['de']:
+            pron_blocks = re.findall("(?m){{IPA}} {{Lautschrift\|([^}]+)}}(?:[^{]+{{Lautschrift\|([^}]+)}})?", entry_block)
+            prons = {_refine_pron(pron) for pron_block in pron_blocks for pron in pron_block if len(pron) > 0 and "..." not in pron and "…" not in pron}
+        elif lcode in ['it', 'fi', 'pl']:
+            pron_blocks = re.findall("(?m){{IPA\d?\|([^}]+)", entry_block)
+            prons = {_refine_pron(pron) for pron_block in pron_blocks for pron in pron_block.split("|")}
+        elif lcode in ['es']:
+            pron_blocks = re.findall("(?m){{pron-graf\|([^}]+)", entry_block)
+            prons = {_refine_pron(re.sub("^[^=]+=", "", pron)) for pron_block in pron_blocks for pron in pron_block.split("|") if 'fone=' in pron}
+        elif lcode in ['fr']:
+            pron_blocks = re.findall("(?m){{pron\|([^}\|]+)", entry_block)
+            prons = {_refine_pron(pron_block) for pron_block in pron_blocks}
+        return list(prons)
+    except:
+        return None
+
 def build_corpus():
-    global lcode, fname, ns
-    
     # Create folder
     if not os.path.exists('data/refined'): os.mkdir('data/refined')
     
@@ -89,15 +98,14 @@ def build_corpus():
     results = []
     i = 1
     for _, elem in ET.iterparse("data/raw/{}".format(fname), tag=ns+"page"):
-        hws = _get_hws(elem)
+        hw = _get_hw(elem)
         prons = _get_prons(elem)
-        if hws and prons:
-            for hw in hws:
-                for pron in prons:
-                    results.append((hw, pron))
+        if hw and prons:
+            for pron in prons:
+                results.append((hw, pron))
         
         elem.clear() # We need to save memory!
-        if i % 1000 == 0: print(i, end="===")
+        if i % 1000 == 0: print(i)
         i += 1
     
     # Write to file    
@@ -105,7 +113,7 @@ def build_corpus():
         fout.write("headword,pronunciation\n")
         results.sort(key=lambda x:x[0])
         for headword, pron in results:
-            fout.write(u"{},{}\n".format(headword, pron))
+            fout.write("{},{}\n".format(headword, pron))
 
 if __name__ == "__main__":
     build_corpus()
